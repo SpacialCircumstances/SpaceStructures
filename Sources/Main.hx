e81ec783662ff0546756b8cdfe7dcef6709d85ef
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-06-18 19:04:17 +0200

    HTML5 build

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -2,8 +2,10 @@ package;
 
 import kha.System;
 
-class Main {
-	public static function main() {
+class Main 
+{
+	public static function main() 
+	{
 		System.init({title: "Space Structures", width: 1200, height: 900}, function () {
 			new Project();
 		});
--- Sources/Project.hx
+++ Sources/Project.hx
@@ -7,31 +7,35 @@ import kha.Color;
 import kha.Image;
 import kha.Scaler;
 
-class Project {
+class Project 
+{
 	public static inline var screenWidth = 1200;
     public static inline var screenHeight = 900;
 
 	private var backbuffer: Image;
 	private var bg: Color;
-	public function new() {
+	public function new() 
+	{
 		System.notifyOnRender(render);
 		Scheduler.addTimeTask(update, 0, 1 / 60);
 		backbuffer = Image.createRenderTarget(screenWidth, screenHeight);
 		bg = Color.Blue;
 	}
 
-	function update(): Void {
+	function update(): Void 
+	{
 		
 	}
 
-	function render(framebuffer: Framebuffer): Void {	
-			var g = backbuffer.g2;
+	function render(framebuffer: Framebuffer): Void
+	{	
+		var g = backbuffer.g2;
 
-	g.begin(bg);
-    g.end();
+		g.begin(bg);
+	    g.end();
 
-    framebuffer.g2.begin();
-    Scaler.scale(backbuffer, framebuffer, System.screenRotation);
-    framebuffer.g2.end();
+    	framebuffer.g2.begin();
+    	Scaler.scale(backbuffer, framebuffer, System.screenRotation);
+    	framebuffer.g2.end();
 	}
 }
