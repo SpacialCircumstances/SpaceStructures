b21e1a74be0f003623888e1bd58df64f327e6f2e
SpacialCircumstances <SpacialCircumstances@users.noreply.github.com>
2017-06-19 11:26:01 +0200

    Add gradients

---
+++ Sources/Gradient.hx
@@ -0,0 +1,14 @@
+package;
+
+import kha.Color;
+
+class Gradient
+{
+	public var value: Float;
+	public var color: Color;
+	public function new(v: Float, c: Color)
+	{
+		color = c;
+		value = v;
+	}
+}
---
+++ Sources/GradientMap.hx
@@ -0,0 +1,20 @@
+package;
+
+import kha.Color;
+
+class GradientMap
+{
+	var map: List<Gradient>;
+	public function new()
+	{
+		map = new 
+	}
+	public function add(g: Gradient): Void
+	{
+		map.add(g);
+	}
+	public function getColor(f: Float): Color
+	{
+		return Color.Black;
+	}
+}
